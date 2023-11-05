47b4a674d75be993cda92799df281a794cd895ad
yoonsion0719 <yoonsonia0719@gmail.com>
2023-11-05 20:30:52 +0900

    09-2

--- main.c
+++ main.c
@@ -2,23 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+#define SIZE 5
 int main(void) {
 	
 	int i;
-	int grade[5];
+	int grade[SIZE];
+	printf("5명의 점수를 입력하시오. \n");
 	
-	grade[0] =10;
-	grade[1] =20;
-	grade[2] =30;
-	grade[3] =40;
-	grade[4] =50;
-	
-	for (i=0;i<5;i++)
-	{
+	for (i=0;i<SIZE;i++)
+		scanf("%d",&grade[i]);
+		
+	for (i=0;i<SIZE;i++)
 		printf("grade[%d] = %d\n",i,grade[i]);
-	}
-	
 	
 	return 0;
 }
